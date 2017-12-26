5ed5f3f5466b7be7ed4470b702d5164b9e0269b5
flenga <fabilenga@gmail.com>
2017-12-26 23:08:33 +0200

    Commit to new repository

--- Final_Project/src/pageObjects/HeaderAreaPage.java
+++ Final_Project/src/pageObjects/HeaderAreaPage.java
@@ -2,7 +2,6 @@ package pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-
 import projectUtilities.BaseFunction;
 import projectUtilities.CommonFunction;
 
